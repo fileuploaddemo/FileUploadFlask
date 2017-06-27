fec56e31af9a26a1f56878e6a1b5deec67a58827
codeyu <codeyu@gmail.com>
2017-06-27 22:53:28 +0800

    0.1

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template, jsonify
-
+from flask import Flask, render_template, jsonify, send_from_directory, request
+from werkzeug.utils import secure_filename
+import os
+import uuid
 app = Flask(__name__)
+
+UPLOAD_FOLDER='upload'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+basedir = os.path.abspath(os.path.dirname(__file__))
+ALLOWED_EXTENSIONS = set(['txt','png','jpg','gif','pdf','epub'])  
+IGNORED_FILES = set(['.gitignore'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
+
+def init_dir():
+    file_dir=os.path.join(basedir,app.config['UPLOAD_FOLDER'])
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+    return file_dir
 
 @app.route('/')
 def index():
@@ -8,33 +25,48 @@ def index():
 
 @app.route('/files')
 def load():
-    files = [
-        {
-          "id": "1",
-          "size": 100,
-          "name": "python",
-          "path": "/books/python"
-        },
-        {
-          "id": "2",
-          "size": 200,
-          "name": "golang",
-          "path": "/books/golang"
-        }
-    ]
-    return jsonify(files)
+    file_dir = init_dir()
+    files_saved = [f for f in os.listdir(file_dir) if os.path.isfile(os.path.join(file_dir, f)) and f not in IGNORED_FILES ]
+    files = []
+    number = 0
+    for f in files_saved:
+        size = os.path.getsize(os.path.join(app.config['UPLOAD_FOLDER'], f))
+        number += 1
+        files.append({
+          "id": number,
+          "size": size,
+          "name": f,
+          "path": ""
+        })
 
-@app.route('/files/<name>')
-def download(name):
-    return "todo"
+    return jsonify(files)    
+
+@app.route('/files/<string:filename>')
+def download(filename):
+    return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER']), filename=filename, as_attachment=True)
 
 @app.route('/files', methods=['POST'])
 def upload():
-    return "todo"
+    file_dir = init_dir()
+    file = request.files['newfile']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(file_dir, filename))
+        return jsonify({"result":"OK"})
+    else:
+        return jsonify({"result":"error"})
 
-@app.route('/files/<name>', methods=['DELETE'])
-def delete(name):
-    return "todo"
+@app.route("/files/<string:filename>", methods=['DELETE'])
+def delete(filename):
+    file_dir = init_dir()
+    file_path = os.path.join(file_dir, filename)
+
+    if os.path.exists(file_path):
+        try:
+            os.remove(file_path)            
+            return jsonify({"result":"OK"})
+        except:
+            return jsonify({"result":"error"})
 
 
 
